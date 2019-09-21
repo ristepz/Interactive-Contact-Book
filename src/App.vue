b8b32ebedba77e8c566115dd9cb621dfd906778a
Riste Pazarkoski <r.pazarkoski@btobet.com>
2019-09-21 14:59:23 +0200

    ongoing changes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <Card @oncardvalue="writeCardVal" :val="cardValue" v-if="showCard" />
-    {{cardValue}}
-    <button @click="showCard = false">Set Card Value</button>
   </div>
 </template>
 
 <script>
-import Card from "./components/Card";
 export default {
   name: "app",
   data() {
     return {
-      cardValue: "",
-      showCard: true
     };
   },
   methods: {
-    writeCardVal(v) {
-      this.cardValue = v;
-    }
   },
   components: {
-    Card
   }
 };
 </script>
